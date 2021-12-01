895935a4b82926ca76734ebb406f165a9bae4df9
vvcaw <eric.elflein@googlemail.com>
2021-12-01 19:27:43 +0100

    Update variables to be float

--- Particle.h
+++ Particle.h
@@ -1,9 +1,9 @@
 #pragma once
 struct Particle {
-    double x;
-    double y;
-    double m;
-    double r;
-    double vx;
-    double vy;
+    float x;
+    float y;
+    float m;
+    float r;
+    float vx;
+    float vy;
 };
